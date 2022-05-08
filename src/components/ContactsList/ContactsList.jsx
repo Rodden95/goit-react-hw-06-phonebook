f8354b4209521ecee19490ee49b2cca2c3f1c118
Rodden95 <rodin0911@gmail.com>
2022-05-08 17:46:43 +0200

    1234

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -22,7 +22,8 @@ export default function ContactsList() {
                 id={item?.id}
                 name={item?.name}
                 phone={item.phone} />
-            </ListGroup.Item>)
+            </ListGroup.Item>
+          )
           :
           <h3>No matches</h3>}
     </ListGroup>
